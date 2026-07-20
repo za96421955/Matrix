c14944f1efa9c81904a445a86c99f45b97d8b108
chenc125 <96421955@163.com>
2026-07-20 19:34:07 +0800

    local

--- matrix-local/src/main/java/com/matrix/local/LocalApplication.java
+++ matrix-local/src/main/java/com/matrix/local/LocalApplication.java
@@ -1,37 +1,14 @@
 package com.matrix.local;
 
-import com.matrix.client.ClientApplication;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.FilterType;
 
 @SpringBootApplication
 @MapperScan(basePackages = {
         "com.matrix.service.dal.mapper",
         "com.matrix.local.dal.mapper"
 })
-@ComponentScan(
-        basePackages = {
-                "com.matrix.service",
-                "com.matrix.local",
-                "com.matrix.client",
-                "com.matrix.common"
-        },
-        excludeFilters = {
-                @ComponentScan.Filter(
-                        type = FilterType.REGEX,
-                        pattern = {
-                                "com\\.matrix\\.service\\.mqtt\\..*"
-                        }
-                ),
-                @ComponentScan.Filter(
-                        type = FilterType.ASSIGNABLE_TYPE,
-                        value = ClientApplication.class
-                )
-        }
-)
 public class LocalApplication {
 
     public static void main(String[] args) {

3c0a58ba0d7ceb1096fe12f42a1b1011d1d8a5e0
chenc125 <96421955@163.com>
2026-07-23 09:47:51 +0800

    1.0.2

--- install/install.sh
+++ install/install.sh
@@ -53,8 +53,8 @@ write_to_profiles() {
 }
 
 # ---------- 下载 URL 常量 ----------
-SERVER="https://gitee.com/za96421955/matrix/raw/release/1.0.1/install"
-RELEASE_URL="https://gitee.com/za96421955/matrix/releases/download/v1.0.2"
+SERVER="https://gitee.com/za96421955/matrix/raw/release/1.0.2/install"
+RELEASE_URL="https://gitee.com/za96421955/matrix/releases/download/v1.0.1"
 
 # ---------- 系统架构检测 ----------
 OS=$(uname -s)
